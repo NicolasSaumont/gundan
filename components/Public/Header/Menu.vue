e85acfd5ec45d2e95b31d6aa86f69826ccf9eecb
Nicolas Saumont <saumont.nicolas@gmail.com>
2024-12-14 15:13:03 +0100

    feat: add style

--- components/Public/Header/Menu.vue
+++ components/Public/Header/Menu.vue
@@ -5,6 +5,7 @@ const scrollTo = (id: string) => {
   const element = document.getElementById(id);
   if (!element) return;
 
+  // Ajustement de la position vers laquelle scroller pour prendre en compte le sticky header
   const targetPosition = element.offsetTop;
 
   const headerHeight = document.getElementById('header')?.offsetHeight || 0;
@@ -14,21 +15,21 @@ const scrollTo = (id: string) => {
   // Défilement vers la position ajustée
   window.scrollTo({
     top: adjustedPosition,
-    behavior: 'smooth', // Ou 'auto' pour sans animation
+    behavior: 'smooth'
   });
 };
 </script>
 
 <template>
-  <div class="flex gap-2">
-    <p class="link" @click="scrollTo('top')">{{ t('Accueil') }}</p>
-    <p class="link" @click="scrollTo('game')">{{ t('Jeu') }}</p>
-    <p class="link" @click="scrollTo('legions')">{{ t('Légions') }}</p>
+  <div class="tracking-wider flex gap-6 text-white font-semibold">
+    <p class="link p-2 rounded-lg" @click="scrollTo('top')">{{ t('Accueil') }}</p>
+    <p class="link p-2 rounded-lg" @click="scrollTo('game')">{{ t('Jeu') }}</p>
+    <p class="link p-2 rounded-lg" @click="scrollTo('legions')">{{ t('Légions') }}</p>
   </div>
 </template>
 
 <style>
 .link {
-  @apply hover:cursor-pointer
+  @apply hover:cursor-pointer hover:bg-white hover:tracking-widest hover:shadow-2xl hover:text-secondary focus:outline-none focus:ring-2 focus:ring-secondary active:text-secondary transition-all duration-200 ease-in-out;
 }
 </style>
