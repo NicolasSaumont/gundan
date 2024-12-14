c26eb01e75091422127ab8917355f5d07576c9f8
Nicolas Saumont <saumont.nicolas@gmail.com>
2024-12-14 15:33:28 +0100

    refactor: add button component + useMenu composable

--- components/Public/Header/Menu.vue
+++ components/Public/Header/Menu.vue
@@ -1,35 +1,11 @@
 <script setup lang="ts">
-const { t } = useI18n()
-
-const scrollTo = (id: string) => {
-  const element = document.getElementById(id);
-  if (!element) return;
-
-  // Ajustement de la position vers laquelle scroller pour prendre en compte le sticky header
-  const targetPosition = element.offsetTop;
-
-  const headerHeight = document.getElementById('header')?.offsetHeight || 0;
-
-  const adjustedPosition = targetPosition - headerHeight - GAP_BETWEEN_SECTIONS;
-
-  // Défilement vers la position ajustée
-  window.scrollTo({
-    top: adjustedPosition,
-    behavior: 'smooth'
-  });
-};
+const { scrollTo } = useMenu()
 </script>
 
 <template>
-  <div class="tracking-wider flex gap-6 text-white font-semibold">
-    <p class="link p-2 rounded-lg" @click="scrollTo('top')">{{ t('Accueil') }}</p>
-    <p class="link p-2 rounded-lg" @click="scrollTo('game')">{{ t('Jeu') }}</p>
-    <p class="link p-2 rounded-lg" @click="scrollTo('legions')">{{ t('Légions') }}</p>
+  <div class="flex gap-6">
+    <SharedButton :text-content="'Accueil'" @click="scrollTo('top')" />
+    <SharedButton :text-content="'Jeu'" @click="scrollTo('game')" />
+    <SharedButton :text-content="'Légions'" @click="scrollTo('legions')" />
   </div>
 </template>
-
-<style>
-.link {
-  @apply hover:cursor-pointer hover:bg-white hover:tracking-widest hover:shadow-2xl hover:text-secondary focus:outline-none focus:ring-2 focus:ring-secondary active:text-secondary transition-all duration-200 ease-in-out;
-}
-</style>
--- components/Shared/Button.vue
+++ components/Shared/Button.vue
@@ -1,3 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{
+  textContent: string
+}>()
+const { t } = useI18n()
+</script>
 
-<template></template>
+<template>
+  <div
+    class="tracking-widest text-white font-semibold px-3 py-2 rounded-lg hover:cursor-pointer hover:bg-white hover:shadow-2xl hover:text-secondary focus:outline-none focus:ring-2 focus:ring-secondary active:text-secondary transition-all duration-200 ease-in-out">
+    {{ t(textContent) }}
+  </div>
+</template>
