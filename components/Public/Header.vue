5ce9a18b862ad52e2c7aa84c6f3621958f23c071
Nicolas Saumont <saumont.nicolas@gmail.com>
2024-12-15 20:55:54 +0100

    feat: add alt on logo image

--- components/Public/Header.vue
+++ components/Public/Header.vue
@@ -4,7 +4,9 @@ const { t } = useI18n()
 
 <template>
   <header class="flex justify-between items-center bg-primary -mt-5 -mx-5 p-5 sticky top-0 left-0">
-    <a href="/" :title="t('Accueil Gundan')"><img src="~/public/logo.jpg" width="100px" class="rounded-xl shadow-lg"></a>
+    <a href="/" :title="t('Accueil Gundan')" :alt="t('Logo de l\'application Gundan')">
+      <img src="~/public/logo.jpg" width="100px" class="rounded-xl shadow-lg">
+    </a>
     <PublicHeaderMenu />
   </header>
 </template>
