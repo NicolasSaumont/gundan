952df536f1f055ee17fea6b73ded5fd7d1e28c19
Nicolas Saumont <saumont.nicolas@gmail.com>
2024-12-18 21:05:22 +0100

    feat: change image position

--- components/Public/GameSection/Card.vue
+++ components/Public/GameSection/Card.vue
@@ -7,8 +7,8 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <article class="flex flex-col gap-6 items-center bg-black w-full border-2 p-4 border-secondary rounded-lg">
-    <img :src="`/_nuxt/assets/images/${card.sourceImage}`" :alt="t('Image de présentation du jeu')" class="h-48">
+  <article class="flex flex-col gap-6 items-center bg-black w-full border-2 p-4 pb-8 border-secondary rounded-lg">
+    <img :src="`/_nuxt/assets/images/${card.sourceImage}`" :alt="t('Image de présentation du jeu')" class="h-48 -mt-16">
     <h2 class="font-caveat text-6xl">{{ card.title }}</h2>
     <p class="text-center">{{ card.content }}</p>
   </article>
