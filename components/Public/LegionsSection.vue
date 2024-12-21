92d54bb6b66084bd7d5352824c5e22ff82ad55a4
Nicolas Saumont <saumont.nicolas@gmail.com>
2024-12-21 10:44:33 +0100

    feat: add legions logo

--- components/Public/LegionsSection.vue
+++ components/Public/LegionsSection.vue
@@ -1,25 +1,25 @@
 <script setup lang='ts'>
 const { t } = useI18n()
 
-const sliderIndex = ref(1)
+const sliderIndex = ref(2)
 
 const legions: ILegions = [
   {
     id: 1,
     name: 'One Piece',
-    image: 'https://example.com/legion-1.png',
+    image: `${LOGO_FILE}/one-piece.png`,
     description: t('La légion One Piece rassemble les héros et pirates les plus célèbres de l’univers du célèbre manga. En combinant force, stratégie et camaraderie, cette légion excelle dans les attaques dévastatrices et les capacités spéciales. Les personnages emblématiques comme Luffy, Zoro et Nami apportent chacun des pouvoirs uniques, capables de retourner le cours d’un combat. Grâce à des synergies de légion puissantes, les pirates de One Piece peuvent se soutenir mutuellement pour infliger des dégâts massifs ou renforcer la défense de l’équipage. Utilisez leur esprit d’équipe et leur détermination pour dominer vos adversaires et atteindre votre objectif : le trésor ultime !'),
   },
   {
     id: 2,
     name: 'Naruto',
-    image: 'https://example.com/legion-1.png',
+    image: `${LOGO_FILE}/naruto.png`,
     description: t('La légion Naruto regroupe les ninjas les plus puissants de l’univers de Konoha et au-delà. Des héros comme Naruto, Sasuke et Sakura offrent une combinaison redoutable de techniques secrètes et de capacités stratégiques. Les personnages de cette légion maîtrisent des pouvoirs uniques, comme le chakra et les transformations, qui peuvent infliger des dégâts massifs ou renforcer la défense de votre équipe. Grâce à des bonus de légion activés par les synergies entre les personnages, la légion Naruto excelle dans l’adaptation et la résilience. Utilisez leurs compétences pour contourner les obstacles et dominer vos ennemis dans chaque combat !'),
   },
   {
     id: 3,
     name: 'Dragon Ball',
-    image: 'https://example.com/legion-1.png',
+    image: `${LOGO_FILE}/dragon-ball.png`,
     description: t('La légion Dragon Ball réunit les guerriers les plus puissants de l’univers, des Saiyans comme Goku et Vegeta aux maîtres d\'arts martiaux comme Piccolo et Gohan. Avec leur force incroyable et leurs techniques légendaires comme le Kamehameha et le Final Flash, ces héros peuvent déclencher des attaques dévastatrices qui changent le cours de la bataille. Grâce aux synergies de légion, les personnages de Dragon Ball peuvent renforcer leurs attaques ou défendre avec une puissance inouïe. Exploitez leur potentiel infini, activez leurs capacités spéciales et faites face à tous vos adversaires avec la fureur des guerriers Saiyans !'),
   }
 ]
