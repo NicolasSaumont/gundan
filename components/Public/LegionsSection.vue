03d9c15fad9c50417c0a19614bb248adafb3e99a
Nicolas Saumont <saumont.nicolas@gmail.com>
2024-12-22 09:52:32 +0100

    feat: adjust section height

--- components/Public/LegionsSection.vue
+++ components/Public/LegionsSection.vue
@@ -26,12 +26,12 @@ onMounted(async () => {
 </script>
 
 <template>
-  <section id="legions" class="h-[850px] bg-primary -mx-5">
+  <section id="legions" class="h-[650px] bg-primary -mx-5">
     <div class="main-container slider flex flex-col items-center gap-2 px-10 py-10 h-full">
       <div class="main-container slider flex gap-4 px-10 py-10 h-full">
         <div class="w-1/3 p-4 flex flex-col justify-around">
-          <h2 class="font-caveat text-6xl text-secondary">{{ t('Créez la plus puissante des alliances') }}</h2>
-          <p>{{ t('Dans GUNDAN, les joueurs peuvent combiner plusieurs légions issues des mangas les plus cultes. Chaque légion apporte ses héros uniques, avec des pouvoirs et des synergies spéciales, permettant de créer une stratégie sur-mesure. Les bonus de légion activent des effets puissants selon les personnages choisis, et leur interaction crée des dynamiques de jeu inédites. Choisissez judicieusement vos légions, exploitez leurs forces et menez vos héros à la victoire dans ce jeu de cartes où stratégie et alliances sont la clé du succès !') }}</p>
+          <h2 class="font-caveat text-4xl text-secondary">{{ t('Créez la plus puissante des alliances') }}</h2>
+          <p>{{ t('Dans GUNDAN, combinez des légions issues des mangas cultes pour créer des stratégies uniques. Chaque légion offre des héros aux pouvoirs et synergies spéciales. Activez des bonus puissants, exploitez leurs forces et menez vos héros à la victoire dans ce jeu de cartes où stratégie et alliances font la différence !') }}</p>
         </div>
         <template v-for="legion in legions" :key="legion.id">
           <PublicLegionsSectionLegion v-show="sliderIndex === legion.id" :legion="legion" />
