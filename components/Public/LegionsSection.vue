01083b04ca22fe3dc25c88bc5f05521a12e58646
Nicolas Saumont <saumont.nicolas@gmail.com>
2024-12-22 21:40:21 +0100

    feat: animate legion changes (wip)

--- components/Public/LegionsSection.vue
+++ components/Public/LegionsSection.vue
@@ -6,9 +6,31 @@ const sliderIndex = ref(1)
 
 onMounted(async () => {
   await setLegions()
-  // Initialisae sliderIndex à une valeur aléatoire entre le premier et le dernier index des légions
+  // Initialise sliderIndex à une valeur aléatoire entre le premier et le dernier index des légions
   sliderIndex.value = Math.floor(Math.random() * legions.length) + 1;
 })
+
+const isFlipping = ref(false); // État pour gérer l'animation
+
+// Fonction pour animer le retournement et changer de légion
+const flipCard = (direction: "next" | "previous") => {
+  if (isFlipping.value) return; // Empêche une double animation
+  isFlipping.value = true;
+
+  // Attendre que la carte soit "retournée" pour changer l'index
+  setTimeout(() => {
+    if (direction === "next") {
+      sliderIndex.value = sliderIndex.value !== legions.length ? sliderIndex.value + 1 : 1;
+    } else {
+      sliderIndex.value = sliderIndex.value !== 1 ? sliderIndex.value - 1 : legions.length;
+    }
+  }, 300); // Correspond à mi-parcours de l'animation
+
+  // Terminer l'animation après la rotation complète
+  setTimeout(() => {
+    isFlipping.value = false;
+  }, 600); // Durée totale de l'animation
+};
 </script>
 
 <template>
@@ -19,11 +41,16 @@ onMounted(async () => {
           <h2 class="font-caveat text-4xl text-secondary">{{ t('Créez la plus puissante des alliances') }}</h2>
           <p>{{ t('Dans GUNDAN, combinez des légions issues des mangas cultes pour créer des stratégies uniques. Chaque légion offre des héros aux pouvoirs et synergies spéciales. Activez des bonus puissants, exploitez leurs forces et menez vos héros à la victoire dans ce jeu de cartes où stratégie et alliances font la différence !') }}</p>
         </div>
-        <template v-for="legion in legions" :key="legion.id">
-          <PublicLegionsSectionLegion v-show="sliderIndex === legion.id" :legion="legion" />
-        </template>
+        <div
+          class="card-container"
+          :class="{ flipping: isFlipping }"
+        >
+          <template v-for="legion in legions" :key="legion.id">
+            <PublicLegionsSectionLegion v-show="sliderIndex === legion.id" :legion="legion" />
+          </template>
+        </div>
       </div>
-      <PublicLegionsSectionNavigation v-model:slider-index="sliderIndex"/>
+      <PublicLegionsSectionNavigation @slider-index-next="() => flipCard('next')" @slider-index-previous="() => flipCard('previous')" />
     </div>
   </section>
 </template>
@@ -48,5 +75,25 @@ onMounted(async () => {
   50% { transform: translate(1px, -1px); }
   75% { transform: translate(-0.5px, -0.5px); }
   100% { transform: translate(0px, 0px); }
+}
+
+.card-container {
+  perspective: 1000px;
+}
+
+.card-container.flipping {
+  animation: flip 0.6s ease-in-out;
+}
+
+@keyframes flip {
+  0% {
+    transform: rotateY(0deg);
+  }
+  50% {
+    transform: rotateY(90deg);
+  }
+  100% {
+    transform: rotateY(0deg);
+  }
 }
 </style>
--- components/Public/LegionsSection/Navigation.vue
+++ components/Public/LegionsSection/Navigation.vue
@@ -1,24 +1,27 @@
 <script setup lang='ts'>
-const sliderIndex = defineModel<number>('sliderIndex', { required: true })
+//const sliderIndex = defineModel<number>('sliderIndex', { required: true })
 
 const { legions } = useLegionsStore()
 const { t } = useI18n()
 
-const handlePreviousLegionClick = () => {
-  if (sliderIndex.value !== 1) {
-    sliderIndex.value -= 1
-  } else {
-    sliderIndex.value = legions.length
-  }
-} 
+//const handlePreviousLegionClick = () => {
+//  if (sliderIndex.value !== 1) {
+//    sliderIndex.value -= 1
+//  } else {
+//    sliderIndex.value = legions.length
+//  }
+//} 
 
-const handleNextLegionClick = () => {
-  if (sliderIndex.value !== legions.length) {
-    sliderIndex.value += 1
-  } else {
-    sliderIndex.value = 1
-  }
-} 
+//const handleNextLegionClick = () => {
+//  if (sliderIndex.value !== legions.length) {
+//    sliderIndex.value += 1
+//  } else {
+//    sliderIndex.value = 1
+//  }
+//} 
+
+defineEmits(["slider-index-next", "slider-index-previous"]);
+
 </script>
 
 <template>
@@ -27,14 +30,14 @@ const handleNextLegionClick = () => {
       :alt="t('Flèche de sélection de la légion précédente')" 
       :title="t('Légion précédente')"
       class="cursor-pointer shaking-animation"
-      @click="handlePreviousLegionClick"
+      @click="$emit('slider-index-previous')"
     >
     <img 
       src="@/assets/images/arrowRight.png" 
       :alt="t('Flèche de sélection de la légion suivante')" 
       :title="t('Légion suivante')"
       class="cursor-pointer shaking-animation animation-delay-50"
-      @click="handleNextLegionClick"
+      @click="$emit('slider-index-next')"
     >
   </nav>
 </template>
