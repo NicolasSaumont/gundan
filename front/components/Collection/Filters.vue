f6b3720ed57eca9e62ca4c7014eece2e05203e52
nicolas.saumont <nicolas.saumont@cooperl.com>
2025-02-03 20:49:30 +0100

    refactor: clean filters code

--- front/components/Collection/Filters.vue
+++ front/components/Collection/Filters.vue
@@ -7,6 +7,16 @@ const gameMode = defineModel<GameModeType>('gameMode', { required: true })
 
 const { t } = useI18n()
 
+const icons = [
+  { mode: "quick", icon: faBolt, color: "text-secondary" },
+  { mode: "classical", icon: faChess, color: "text-tertiary" },
+]
+
+const modes = [
+  { value: "classical", label: "Mode classique", position: "right-4" },
+  { value: "quick", label: "Mode rapide", position: "left-4" },
+]
+
 const toggleGameMode = () => {
   gameMode.value = gameMode.value === 'quick' ? 'classical' : 'quick'
 }
@@ -15,7 +25,7 @@ const toggleGameMode = () => {
 <template>
   <div class="bg-white shadow-inner-medium border-2 border-primary rounded-md p-4">
     <button
-      class="text-white relative w-40 h-10 bg-primary shadow-inner-strong rounded-full p-1 flex items-center transition-all duration-300"
+      class="relative flex items-center w-40 h-10 bg-primary shadow-inner-strong rounded-full text-white p-1 transition-all duration-300"
       :class="{ 'bg-tertiary': gameMode === 'quick' }"
       @click="toggleGameMode"
     >
@@ -23,30 +33,22 @@ const toggleGameMode = () => {
         class="flex items-center justify-center w-8 h-8 bg-white shadow-inner-medium rounded-full transform transition-transform duration-300"
         :class="{ 'translate-x-[120px]': gameMode === 'quick' }"
       >
-        <font-awesome-icon 
-          :icon="faBolt" 
-          size="lg" 
-          class="text-secondary absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300" 
-          :class="{ 'opacity-0': gameMode === 'classical' }"
-        />
-        <font-awesome-icon 
-          :icon="faChess" 
-          size="lg" 
-          class="text-tertiary absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300" 
-          :class="{ 'opacity-0': gameMode === 'quick' }"
+        <font-awesome-icon
+          v-for="icon in icons"
+          :key="icon.mode"
+          :icon="icon.icon"
+          size="lg"
+          class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300"
+          :class="[icon.color, { 'opacity-0': gameMode !== icon.mode }]"
         />
       </div>
       <span
-        class="text-sm absolute right-4 transition-opacity duration-300"
-        :class="{ 'opacity-0': gameMode === 'quick' }"
+        v-for="mode in modes"
+        :key="mode.value"
+        class="text-sm absolute transition-opacity duration-300"
+        :class="[mode.position, { 'opacity-0': gameMode !== mode.value }]"
       >
-        {{ t("Mode classique") }}
-      </span>
-      <span
-        class="text-sm absolute left-4 transition-opacity duration-300"
-        :class="{ 'opacity-0': gameMode === 'classical' }"
-      >
-        {{ t("Mode rapide") }}
+        {{ t(mode.label) }}
       </span>
     </button>
   </div>
