271a2c01819640d1e2a07211809b2ff748a76128
nicolas.saumont <nicolas.saumont@cooperl.com>
2025-02-10 21:18:42 +0100

    feat: add hover character image

--- front/components/Collection/List/Card.vue
+++ front/components/Collection/List/Card.vue
@@ -3,6 +3,8 @@ const props = defineProps<{
   card: ICard
 }>()
 
+const { t } = useI18n()
+
 const { formatLegionName } = useCard()
 
 const legionName = ref<string>()
@@ -21,23 +23,8 @@ onMounted(() => {
     @mouseenter="isHovered = true"
     @mouseleave="isHovered = false"
   >
-    <!-- Back Shadow -->
-    <div 
-      class="absolute inset-0 w-full h-full transition-opacity duration-500 ease-in-out pointer-events-none"
-      :class="{ 'opacity-100': isHovered, 'opacity-0': !isHovered }"
-      style="
-        content: '';
-        position: absolute;
-        top: 87%;
-        left: 50%;
-        width: 102%;
-        height: 12%;
-        background: rgba(255, 255, 255, 0.4);
-        filter: blur(10px);
-        transform: translateX(-50%);
-        transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
-      "
-    />
+    <CollectionListCardBackShadow :is-hovered="isHovered" />
+
     <!-- Rotation -->
     <div
       class="w-full h-full transition-transform duration-500 ease-in-out"
@@ -66,23 +53,14 @@ onMounted(() => {
   
       <CollectionListCardType :card-type="card.type"/>
 
-      <!-- Front Shadow -->
-      <div 
-        class="absolute inset-0 w-full h-full rounded-2xl transition-opacity duration-500 ease-in-out pointer-events-none"
-        :class="{ 'opacity-100': isHovered, 'opacity-0': !isHovered }"
-        style="
-          content: '';
-          position: absolute;
-          top: 0;
-          left: 50%;
-          width: 100%;
-          height: 100%;
-          background-image: radial-gradient(circle, transparent 100px, black);
-          transform: translateX(-50%);
-          transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
-        "
-      />
+      <CollectionListCardFrontShadow :is-hovered="isHovered" />
     </div>
-    
+
+    <!-- Hover Character Image-->
+    <CollectionListCharacterImage
+      :legion-name="legionName" 
+      :character-image="card.evolution.image"
+      :is-hovered="isHovered"
+    />
   </div>
 </template>
---
+++ front/components/Collection/List/CharacterImage.vue
@@ -0,0 +1,18 @@
+<script setup lang='ts'>
+defineProps<{
+  legionName?: string
+  characterImage: string
+  isHovered?: boolean
+}>()
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <img
+    :src="`_nuxt/assets/images/cards/legions/${legionName}/characters/${characterImage}.png`"
+    :alt="t('Personnage de la carte')"
+    class="aboslute inset-0 transform transition-all duration-500 ease-in-out"
+    :class="{ 'opacity-100 -translate-y-[110%]': isHovered, 'opacity-0 -translate-y-[80%]': !isHovered }"
+  >
+</template>
