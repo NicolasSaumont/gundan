ee6042569864008c5115c20837796679d43a8e48
nicolas.saumont <nicolas.saumont@cooperl.com>
2025-02-11 17:36:11 +0100

    feat: add selected card details

--- front/components/Collection/List/SelectedCardModal.vue
+++ front/components/Collection/List/SelectedCardModal.vue
@@ -1,14 +1,59 @@
 <script setup lang='ts'>
 const isVisible = defineModel<boolean>('isVisible', { required: true })
 
+const { t } = useI18n()
 const { selectedCard } = storeToRefs(useCardStore())
+const { gameMode } = storeToRefs(useCollectionStore())
+
+const isQuickMode = computed(() => gameMode.value === "quick");
+
+const capacity = computed(() => 
+  isQuickMode.value ? selectedCard.value?.skills.capacity.quickMode : selectedCard.value?.skills.capacity.classicalMode
+)
+
+const bonus = computed(() => 
+  isQuickMode.value ? selectedCard.value?.skills.bonus.quickMode : selectedCard.value?.skills.bonus.classicalMode
+)
 </script>
 
 <template>
-  <Modal v-model:is-visible="isVisible">
-    <CollectionListSelectedCardModalCard v-if="selectedCard" :card="selectedCard"/>
-    <div class="bg-white p-4 rounded-lg border border-gray-300">
-      {{ selectedCard?.name }}
+  <Modal v-model:is-visible="isVisible" class="flex gap-4">
+    <div class="flex justify-center gap-4 max-w-[80%] max-h-[70%] mt-[140px]"> <!-- 140px correspond à la hauteur du header -->
+      <CollectionListSelectedCardModalCard v-if="selectedCard" :card="selectedCard" />
+      <div class="flex flex-col gap-4 text-white p-4 max-w-[50%]">
+        <div class="font-bungee text-xl">{{ t(selectedCard?.name ?? '') }}</div>
+        <div>{{ t(selectedCard?.description ?? '') }}</div>
+        <div>
+          <span>{{ t('Type : ') }}</span>
+          <span>{{ t(selectedCard?.type ?? '-') }}</span>
+        </div>
+        <div class="flex flex-col">
+          <div>
+            <span>{{ t('Puissance : ') }}</span>
+            <span>{{ t(selectedCard?.evolution.stats.power ?? '-') }}</span>
+          </div>
+          <div>
+            <span>{{ t('Défense : ') }}</span>
+            <span>{{ t(selectedCard?.evolution.stats.defense ?? '-') }}</span>
+          </div>
+          <div>
+            <span>{{ t('Dégâts : ') }}</span>
+            <span>{{ t(selectedCard?.evolution.stats.damage ?? '-') }}</span>
+          </div>
+          <div>
+            <span>{{ t('Santé : ') }}</span>
+            <span>{{ t(selectedCard?.evolution.stats.health ?? '-') }}</span>
+          </div>
+        </div>
+        <div>
+          <span>{{ t('Capacité : ') }}</span>
+          <span>{{ t(capacity?.description ?? '-') }}</span>
+        </div>
+        <div>
+          <span>{{ t('Bonus : ') }}</span>
+          <span>{{ t(bonus?.description ?? '-') }}</span>
+        </div>
+      </div>
     </div>
   </Modal>
 </template>
--- front/components/Modal.vue
+++ front/components/Modal.vue
@@ -4,15 +4,27 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const isVisible = defineModel<boolean>('isVisible', { required: true })
 
+const closeOnEscape = (event: KeyboardEvent) => {
+  if (event.key === "Escape") {
+    isVisible.value = false;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("keydown", closeOnEscape);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keydown", closeOnEscape);
+});
 </script>
 
 <template>
-  <div v-if="isVisible" class="absolute inset-0 flex justify-center items-center w-full h-full overflow-hidden bg-black/80">
+  <div v-if="isVisible" class="absolute inset-0 flex justify-center items-center w-full h-full overflow-hidden bg-black/95" @click="isVisible = false">
     <font-awesome-icon
       :icon="faXmark"
       size="2xl"
       class="absolute top-40 right-10 text-white cursor-pointer"
-      @click="isVisible = false"
     />
     <slot />
   </div>
