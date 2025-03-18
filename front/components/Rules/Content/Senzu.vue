a907fefa1a04c25cb8e8aa767174bee67a9949e4
nicolas.saumont <nicolas.saumont@cooperl.com>
2025-03-18 13:47:40 +0100

    feat: add senzu

--- front/components/Rules/Content/Senzu.vue
+++ front/components/Rules/Content/Senzu.vue
@@ -1,7 +1,12 @@
-<script setup lang='ts'></script>
+<script setup lang='ts'>
+const { t } = useI18n()
+</script>
 
 <template>
-  <div>
-    Senzu
+  <div class="flex flex-col gap-4">
+    <h3>{{ t('Gestion des Senzu') }}</h3>
+    <div>{{ t('Lors d’un combat, chaque joueur peut investir un certain nombre de Senzu pour augmenter temporairement l’efficacité de son attaque ou de sa défense.') }}</div>
+    <div>{{ t('Chaque joueur dispose d’un certain nombre de Senzu en début de partie (12).') }}</div>
+    <div>{{ t('Les Senzu sont une ressource limitée : une fois dépensés, ils ne peuvent être récupérés que via des capacités spéciales ou des évènements particuliers.') }}</div>
   </div>
 </template>
