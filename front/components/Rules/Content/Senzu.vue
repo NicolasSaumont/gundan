b5f79a8bf885d6ec60146c66da07f33ed68896d0
nicolas.saumont <nicolas.saumont@cooperl.com>
2025-03-21 08:10:13 +0100

    feat: add battle modes

--- front/components/Rules/Content/Senzu.vue
+++ front/components/Rules/Content/Senzu.vue
@@ -8,5 +8,14 @@ const { t } = useI18n()
     <div>{{ t('Lors d’un combat, chaque joueur peut investir un certain nombre de Senzu pour augmenter temporairement l’efficacité de son attaque ou de sa défense.') }}</div>
     <div>{{ t('Chaque joueur dispose d’un certain nombre de Senzu en début de partie (12).') }}</div>
     <div>{{ t('Les Senzu sont une ressource limitée : une fois dépensés, ils ne peuvent être récupérés que via des capacités spéciales ou des évènements particuliers.') }}</div>
+    
+    <h3>{{ t('Mode Super Saiyan') }}</h3>
+    <div>{{ t('Lors de la phase de combat, le joueur peut choisir d\'activer le mode "Super Saiyan" pour un de ses personnages en consommant 3 Senzu. Ce mode confère un puissant boost aux statistiques du personnage :') }}</div>
+    <ul>
+      <li>{{ t('+2 Puissance') }}</li>
+      <li>{{ t('+2 Défense') }}</li>
+      <li>{{ t('+2 Dégâts') }}</li>
+    </ul>
+    <div>{{ t('Le mode Fury dure jusqu\'à la fin du tour et permet au personnage de devenir beaucoup plus résistant et puissant. Cependant, il ne peut être utilisé que si le joueur a suffisamment de Senzu en réserve.') }}</div>
   </div>
 </template>
