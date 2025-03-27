b12c7b993e43a864cb86130abd744bb304a0c505
nicolas.saumont <nicolas.saumont@cooperl.com>
2025-03-27 21:34:30 +0100

    feat: add greedy nami image

--- front/components/Shop/CreditAdd.vue
+++ front/components/Shop/CreditAdd.vue
@@ -1,7 +1,14 @@
-<script setup lang='ts'></script>
+<script setup lang='ts'>
+const { t } = useI18n()
+</script>
 
 <template>
-  <div class="flex border-2 border-white w-full">
-      Credit Add
+  <div class="flex justify-between border-2 border-white w-full gap-4">
+    <div class="m-4">Besoin de plus de crédit ?</div>
+    <img
+      src="@/assets/images/shop-page/greedy-nami.png"
+      :alt="t('Nami sous une pluie de billets')"
+      class="w-96"
+    >
   </div>
 </template>
