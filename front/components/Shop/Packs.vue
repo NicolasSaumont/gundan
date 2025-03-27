a9d3814ab637ff864df38d8dd5c766687ed0c1a5
nicolas.saumont <nicolas.saumont@cooperl.com>
2025-03-27 21:28:24 +0100

    feat: page setup

---
+++ front/components/Shop/Packs.vue
@@ -0,0 +1,7 @@
+<script setup lang='ts'></script>
+
+<template>
+  <div class="flex border-2 border-white w-full">
+      Packs
+  </div>
+</template>
--- front/pages/shop.vue
+++ front/pages/shop.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+const { t } = useI18n()
 </script>
 
 <template>
-  <div>
-    <p>Boutique</p>
+  <div class="flex flex-col items-center text-white main-container h-full gap-10">
+    <h2 class="font-bungee text-lg">{{ t('La boutique') }}</h2>
+    <ShopPacks />
+    <ShopCreditAdd />
   </div>
 </template>
