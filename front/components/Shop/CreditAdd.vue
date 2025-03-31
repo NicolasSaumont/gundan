9387208de3d138070184bb9cc43ebc74a3d4acd4
nicolas.saumont <nicolas.saumont@cooperl.com>
2025-03-31 17:54:59 +0200

    feat: get credits paragraph

--- front/components/Shop/CreditAdd.vue
+++ front/components/Shop/CreditAdd.vue
@@ -1,14 +1,34 @@
 <script setup lang='ts'>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCoins } from '@fortawesome/free-solid-svg-icons/faCoins';
+
 const { t } = useI18n()
+
+const isHovered = ref(false)
 </script>
 
 <template>
-  <div class="flex justify-between border-2 border-white w-full gap-4">
-    <div class="m-4">Besoin de plus de crédit ?</div>
+  <div class="flex justify-between w-full gap-4">
     <img
       src="@/assets/images/shop-page/greedy-nami.png"
       :alt="t('Nami sous une pluie de billets')"
-      class="w-96"
+      class="w-96 border-4 border-white"
     >
+    <div class="grow flex flex-col justify-around items-center gap-4">
+      <div class="flex flex-col items-center gap-2">
+        <div>{{ t('Il te manque quelques crédits pour enfin obtenir le pack de tes rêves ?') }}</div>
+        <div>{{ t('Collecter les crédit grâce aux récompenses de combat est trop long pour toi ?') }}</div>
+        <div>{{ t('Tu en as assez de passer à côté des crédits bonus ?') }}</div>
+      </div>
+      <button
+        class="flex items-center gap-2 bg-primary text-lg text-white border-2 rounded-full px-3 py-2 shadow-xl hover:cursor-pointer hover:bg-black hover:text-secondary hover:border-secondary transition-all duration-300 ease-in-out"
+        :class="{ 'tracking-wider': isHovered, 'tracking-normal': !isHovered }"
+        @mouseenter="isHovered = true"
+        @mouseleave="isHovered = false"
+      >
+        <font-awesome-icon :icon="faCoins" size="xl" />
+        <span>{{ t('Obtiens un max de crédit') }}</span>
+      </button>
+    </div>
   </div>
 </template>
