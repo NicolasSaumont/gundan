76402665e12a37e47a84058f29726c5a1e2aa512
nicolas.saumont <nicolas.saumont@cooperl.com>
2025-04-07 12:47:29 +0200

    feat: add pack reflection

--- front/components/Shop/PacksList/Pack.vue
+++ front/components/Shop/PacksList/Pack.vue
@@ -4,12 +4,36 @@ defineProps<{
 }>()
 
 const { t } = useI18n()
+
+const isHovered = ref(false)
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-between border-2 border-white bg-green-500/20 p-4 h-52 w-44">
+  <div 
+    class="hover-reflection relative overflow-hidden flex flex-col items-center justify-between border-2 border-white bg-green-500/20 p-4 h-52 w-44 cursor-pointer"
+    @mouseenter="isHovered = true"
+    @mouseleave="isHovered = false"
+  >
     <div>{{ pack.price > 1 ? t(pack.price + ' crédits') : t(pack.price + ' crédit') }}</div>
     <div>{{ pack.name }}</div>
     <div>{{ pack.cardsCount > 1 ? t(pack.cardsCount + ' cartes') : t(pack.cardsCount + ' carte') }}</div>
   </div>
 </template>
+
+<style scoped>
+.hover-reflection::before {
+  content: '';
+  @apply absolute top-[-75%] left-[-75%] w-[300%] h-[20%] pointer-events-none transition-transform duration-700 ease-in-out;
+  transform: rotate(135deg);
+  background: linear-gradient(
+    120deg,
+    rgba(255, 255, 255, 0.3) 0%,
+    rgba(255, 255, 255, 0.1) 40%,
+    rgba(255, 255, 255, 0) 80%
+  );
+}
+
+.hover-reflection:hover::before {
+  transform: translateY(1000%) rotate(135deg);
+}
+</style>
