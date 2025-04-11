9d60746f3cfe6d241248d16903aaa255fc97afec
Nicolas Saumont <saumont.nicolas@gmail.com>
2025-04-11 18:28:16 +0200

    feat: close modal when click on cancel button

--- front/components/Shop/PacksList/SelectedPackModal.vue
+++ front/components/Shop/PacksList/SelectedPackModal.vue
@@ -11,16 +11,21 @@ const buyPackText = computed(() => t('Acheter (' + selectedPack.value.price + ' 
 const resetSelectedPack = () => {
   selectedPack.value = DEFAULT_PACK as IPack
 }
+
+const handleCancelClick = () => {
+  resetSelectedPack()
+  isVisible.value = false
+}
 </script>
 
 <template>
   <Modal v-model:is-visible="isVisible" class="flex gap-4" @click="resetSelectedPack">
-    <div class="flex flex-col justify-between gap-2 bg-black border-2 border-white rounded-lg p-4 w-[45%] h-[30%]">
+    <div class="flex flex-col justify-between items-center gap-10 bg-black border-2 border-white rounded-lg p-6" @click.stop>
      <div class="text-center font-bungee">{{ selectedPack.name }}</div>
      <div>{{ selectedPack.description }}</div>
-     <div class="flex gap-4 justify-end">
-        <Button :text="t('Fermer')" flat class="!tracking-normal" />
-        <Button :text="buyPackText" :icon="faCoins" class="!tracking-normal" />
+     <div class="flex gap-4">
+      <Button :text="t('Annuler')" flat class="!tracking-normal" @click="handleCancelClick" />
+      <Button :text="buyPackText" :icon="faCoins" class="!tracking-normal" />
      </div>
     </div>
   </Modal>
