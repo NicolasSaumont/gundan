78f5dfe9980d944a147e1262ea248c1f48faa2a4
Nicolas Saumont <saumont.nicolas@gmail.com>
2025-04-12 13:08:05 +0200

    feat: add button to manage packs quantity

--- front/components/Shop/PacksList/SelectedPackModal.vue
+++ front/components/Shop/PacksList/SelectedPackModal.vue
@@ -2,13 +2,28 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faGem } from '@fortawesome/free-solid-svg-icons/faGem'
 import { faCoins } from '@fortawesome/free-solid-svg-icons/faCoins'
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
+import { faMinus } from '@fortawesome/free-solid-svg-icons/faMinus'
 
 const isVisible = defineModel<boolean>('isVisible', { required: true })
 
 const { t } = useI18n()
 const { selectedPack } = storeToRefs(useShopStore())
 
+const packCount = ref(1)
+const totalPrice = computed(() => selectedPack.value.price * packCount.value )
+
+const incrementPackCount = () => {
+  packCount.value ++
+}
+
+const decrementPackCount = () => {
+  if (packCount.value === 1) return
+  packCount.value --
+}
+
 const resetSelectedPack = () => {
+  packCount.value = 1
   selectedPack.value = DEFAULT_PACK as IPack
 }
 
@@ -23,9 +38,14 @@ const handleCancelClick = () => {
     <div class="flex flex-col justify-between items-center gap-10 bg-black border-2 border-white rounded-lg p-6" @click.stop>
      <div class="text-center font-bungee">{{ selectedPack.name }}</div>
      <div>{{ selectedPack.description }}</div>
+     <div class="flex gap-2 items-center border-2 border-secondary rounded-xl">
+      <font-awesome-icon :icon="faMinus" class="border-r-2 border-secondary p-2 cursor-pointer text-secondary" @click="decrementPackCount" />
+      <div class="px-4">{{ packCount }}</div>
+      <font-awesome-icon :icon="faPlus" class="border-l-2 border-secondary p-2 cursor-pointer text-secondary" @click="incrementPackCount" />
+     </div>
      <div class="flex items-center gap-2">
        <div>{{ t('Coût total :') }}</div>
-       <div>{{ selectedPack.price }}</div>
+       <div>{{ totalPrice }}</div>
        <font-awesome-icon :icon="faGem" size="lg" />
      </div>
      <div class="flex gap-4">
