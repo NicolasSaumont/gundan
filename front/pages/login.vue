6795d0211f976a37ec700ebfa755421a839ccdf1
nicolas.saumont <nicolas.saumont@cooperl.com>
2025-04-14 21:14:07 +0200

    feat: add public layout

--- front/pages/login.vue
+++ front/pages/login.vue
@@ -1,24 +1,31 @@
 <script setup lang='ts'>
+definePageMeta({
+  layout: 'public',
+})
 </script>
 
 <template>
-  <div class="main-container h-full flex items-center justify-center">
-    <div class="relative flex justify-between items-center w-3/4 h-3/4 bg-primary border-4 border-white rounded-xl overflow-hidden">
-      <!-- Image d'arrière-plan avec opacité -->
-      <div 
-        class="absolute inset-0 bg-menu bg-cover bg-no-repeat bg-center opacity-10" 
-      />
+  <div class="flex flex-col h-screen -mt-[40px]" :style="{ gap: `${GAP_BETWEEN_SECTIONS}px` }"> <!-- 40px correspond à 2 fois le padding entourant la div -->
+    <PublicHeader />
+    <div class="h-full flex items-center justify-center">
+      <div class="relative flex justify-between items-center w-3/4 h-3/4 bg-primary border-4 border-white rounded-xl overflow-hidden">
+        <!-- Image d'arrière-plan avec opacité -->
+        <div 
+          class="absolute inset-0 bg-menu bg-cover bg-no-repeat bg-center opacity-10" 
+        />
 
-      <!-- Contenu au-dessus de l'image -->
-      <div class="flex h-full w-full z-10">
+        <!-- Contenu au-dessus de l'image -->
+        <div class="flex h-full w-full z-10">
 
-        <Login />
+          <Login />
 
-        <LoginSeparator />
+          <LoginSeparator />
 
-        <LoginSignup />
-        
+          <LoginSignup />
+          
+        </div>
       </div>
     </div>
+    <PublicFooter/>
   </div>
 </template>
